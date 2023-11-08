314c63462fbc2e14a9443f2b5a5bca0cd30ebc51
HeatherSmallwood <heathersmallwood.hs@gmail.com>
2023-11-07 21:14:48 -0500

    big error

--- app.py
+++ app.py
@@ -15,30 +15,22 @@ def pokemon_form():
             pokemon_url= f"https://pokeapi.co/api/v2/pokemon/{pokemon_data}"
             response = requests.get(pokemon_url)
             extra_data = response.json()
-            new_pokemon_data = get_pokemon_data(extra_data)
-            return render_template('pokemonForm.html', pokemon_data = extra_data)
+            pokemon_dict = {
+                'name' : extra_data['forms'][0]['name'],
+                'ability_name' : extra_data['abilities'][0]['ability']['name'],
+                'ability_name' : extra_data['abilities'][1]['ability']['name'],
+                'base_experience' : extra_data['base_experience'],
+                'attack_base_stat' : extra_data['stats'][1]['base_stat'],
+                'defense_base_stat' : extra_data['stats'][2]['base_stat'],
+                'hp_base_stat' : extra_data['stats'][0]['base_stat'],
+                'sprites_image': extra_data['sprites']['front_shiny']
+            }
+            print(pokemon_dict)
+            return render_template('pokemonForm.html', pokemon_info=pokemon_dict)
         except:
             return render_template('pokemonForm.html')
-        else:
-            return render_template('pokemonForm.html')
+    else:
+        return render_template('pokemonForm.html')
 
 
 
-def get_pokemon_data(pokemon_data):
-    pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_data}"
-    response = requests.get(pokemon_url)
-    response.status_code
-    pokemon_data = response.json()
-    pokemon_dict = {
-            'name' : pokemon_data['forms'][0]['name'],
-            'ability_name' : pokemon_data['abilities'][0]['ability']['name'],
-            'ability_name' : pokemon_data['abilities'][1]['ability']['name'],
-            'base_experience' : pokemon_data['base_experience'],
-            'attack_base_stat' : pokemon_data['stats'][1]['base_stat'],
-            'defense_base_stat' : pokemon_data['stats'][2]['base_stat'],
-            'hp_base_stat' : pokemon_data['stats'][0]['base_stat'],
-            'sprites_image': pokemon_data['sprites']['front_shiny']
-            }
-        
-    
-    return pokemon_dict
