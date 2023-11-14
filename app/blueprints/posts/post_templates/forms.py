fa19cb64e125fbb103d22f6eced316eb59170cfa
HeatherSmallwood <heathersmallwood.hs@gmail.com>
2023-11-14 13:10:46 -0500

    updates

--- app/blueprints/posts/post_templates/forms.py
+++ app/blueprints/posts/post_templates/forms.py
@@ -1,13 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, PasswordField,StringField, EmailField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
 
 
 
 class PostForm(FlaskForm):
     title= StringField('Title: ')
     capition = StringField('Caption: ')
-    img_url = EmailField('Email ', validators=[DataRequired()])
-    password= PasswordField('Password ', validators=[DataRequired()])
-    confirmPassword = PasswordField('Confirm Password ', validators=[DataRequired(), EqualTo('password')])
-    submitButton = SubmitField('Login')
+    img_url = StringField('Image URL: ', validators=[DataRequired()])
+    submitButton = SubmitField('Create Post')
